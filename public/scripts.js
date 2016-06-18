1e14d1830af7daff741018ac511233bb509a4032
Maetz007 <maetz007@yahoo.com>
2016-06-18 11:50:29 -0500

    normal complete minus CSS styling

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,108 @@
 $(document).ready(function(){
 
-  $("btn").on("click", function(){
-  // call some function from server
-  }); // end btn
+$(window).load(function() {
+    tasksDisplay();
+}); // end window load
 
-  function getSomething(){
+//-------------------------------------------------------------------------------------------------------------------
+
+$("#addTaskBtn").on("click", function(){
+  var newTask = $("#taskInput").val();
+  var empty = "";
+    if (newTask == empty){
+      alert("Task input can not be left empty!");
+  } else {
+    var taskObject = {
+      "taskObj": newTask,
+      "completeObj": false
+  }; // end task object
+    $.ajax({
+      type: "POST",
+      url: "/addTaskDatabase",
+      data: taskObject,
+      success: function(){
+        tasksDisplay();
+      } // end success
+    }); // end ajax
+  } // end else
+}); // end add task button
+
+//-------------------------------------------------------------------------------------------------------------------
+
+$("#taskField").on("click", ".deleteBtn", function(){
+  var deleteTaskId = $(this).attr("data-id");
+  var delTask = {
+    "id": deleteTaskId
+    }; // close object
+    $.ajax({
+      type: "POST",
+      url: "/deleteTask",
+      data: delTask,
+      success: function(){
+        $(this).remove();
+        tasksDisplay();
+      } // end success
+    }); // end ajax
+}); // end delete button
+
+//-------------------------------------------------------------------------------------------------------------------
+
+$("#taskField").on("click", "#toggleBtn", function(){
+  var comCheck = $(this).attr("data-complete");
+  var comId = $(this).attr("data-id");
+  var comBoolean;
+  if (comCheck == "true"){
+    comBoolean = false;
+  } else {
+    comBoolean = true;
+  } // end else ifs
+    var comTaskObj = {
+      "objCompleteId": comId,
+      "objCompleteBool": comBoolean
+      }; // close object
+  $.ajax({
+    type: "POST",
+    url: "/completeTask",
+    data: comTaskObj,
+    success: function(){
+      $(this).remove();
+      tasksDisplay();
+    } // end success
+  }); // end ajax
+}); // end complete button
+
+//-------------------------------------------------------------------------------------------------------------------
+
+function tasksDisplay(){
+  document.getElementById("taskField").innerHTML = "";
+  $("#taskInput").attr("placeholder", "Input Task...").val("");
     $.ajax({
       type: "GET",
-      data: data,
-      url: "/somethingGot"
-    });
-  }
-});// end document ready
+      url: "/getAllTasks",
+      success: function(data){
+        for (var i = 0; i < data.length; i++) {
+          var displayTasks = document.createElement("div");
+          displayTasks.id = data[i];
+          displayTasks.className = "displayTaskDiv";
+          var task = "Task: " + data[i].taskname;
+          displayTasks.textContent = task;
+          $("#taskField").append(displayTasks);
+          var deleteBtn = "<button class='deleteBtn' data-id='" + data[i].id + "'>Delete Task" + "</button>";
+          $("#taskField").append(deleteBtn);
+          if (data[i].complete === false){
+            var completeBtn = "<button class='unfinishedBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" +
+            data[i].complete + "'>Task Unfinished" + "</button>";
+              $("#taskField").append(completeBtn);
+          } else {
+            var unfinishedBtn = "<button class='completeBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" +
+            data[i].complete + "'>Completed!" + "</button>";
+              $("#taskField").append(unfinishedBtn);
+          } // end else
+        } // end for loop
+      } // end success
+    }); // end ajax
+} // end taskDisplay
+
+//-------------------------------------------------------------------------------------------------------------------
+
+}); // end document ready
--- server/app.js
+++ server/app.js
@@ -1,16 +1,59 @@
 var express = require("express");
-var app = express;
+var app = express();
 var path = require("path");
-var pg = require("pg");
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+var pg = require("pg");
+var connectionString = 'postgres://localhost:5432/weekFourDatabase';
 
-app.listen(process.env.PORT || 9001, function() {
-  console.log("Listening on port 9001... IT'S OVER 9000!!!");
-});
+
+app.listen(process.env.PORT || 8675, function() {console.log("Listening on port 8675"); });
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-  res.sendFile( path.resolve("views/index.html"));
-});
+app.get("/", function(req, res){
+  console.log("You Are in L");
+  res.sendFile( path.resolve ("views/index.html") );
+}); // end URL app.get
+
+//-------------------------------------------------------------------------------------------------------------------
+
+app.post("/addTaskDatabase", urlencodedParser, function(req, res){
+  pg.connect( connectionString, function( err, client, done ){
+    client.query("INSERT INTO tasks (taskname, complete) VALUES ($1, $2)", [req.body.taskObj, req.body.completeObj]);
+  }); // end pg.connect
+  res.send("res success");
+}); // end /addTaskDatabase
+
+//-------------------------------------------------------------------------------------------------------------------
+
+app.get("/getAllTasks", function(req, res){
+  var dataResult = [];
+  pg.connect( connectionString, function(err, client, done){
+    var callData = client.query("SELECT * FROM tasks;");
+    callData.on('row', function(row){
+      dataResult.push(row);
+    });
+    callData.on('end', function (){
+      return res.json(dataResult);
+    }); // end callData
+  }); // end connect
+}); // end app.get /getUsers
+
+//-------------------------------------------------------------------------------------------------------------------
+
+app.post("/deleteTask", urlencodedParser, function(req, res){
+  pg.connect( connectionString, function( err, client, done ){
+    client.query("DELETE FROM tasks WHERE id = ($1)", [req.body.id]);
+  }); // end pg.connect
+  res.send("res success");
+}); // end /deleteTask
+
+//-------------------------------------------------------------------------------------------------------------------
+
+app.post("/completeTask", urlencodedParser, function(req, res){
+  pg.connect( connectionString, function( err, client, done ){
+    client.query("UPDATE tasks SET complete = ($1) WHERE id = ($2)", [req.body.objCompleteBool, req.body.objCompleteId]);
+  }); // end pg.connect
+  res.send("res success");
+}); // end /completeTask
