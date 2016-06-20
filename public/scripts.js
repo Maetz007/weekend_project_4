861bdb4a5d2f63fc0189c66c7432d93a94421fe4
Maetz007 <maetz007@yahoo.com>
2016-06-19 21:51:58 -0500

    pro mode complete with css styling. Will add comments and clean up later

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
 
+var numUnfinishedTasks = 0;
+var numTasks = 0;
+
+//-------------------------------------------------------------------------------------------------------------------
+
 $(window).load(function() {
-    tasksDisplay();
+  tasksDisplay();
 }); // end window load
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -11,6 +16,8 @@ $("#addTaskBtn").on("click", function(){
   var empty = "";
     if (newTask == empty){
       alert("Task input can not be left empty!");
+  } else if (numTasks > 11){
+      alert("Task power is over 9000!!\n Please delete some tasks before adding new ones.");
   } else {
     var taskObject = {
       "taskObj": newTask,
@@ -81,37 +88,63 @@ function tasksDisplay(){
       type: "GET",
       url: "/getAllTasks",
       success: function(data){
+        numTasks = data.length;
         for (var i = 0; i < data.length; i++) {
           if (data[i].complete === false){
 
             var displayUnfinished = document.createElement("div");
             displayUnfinished.id = data[i];
             displayUnfinished.className = "displayUnfinishedDiv";
-            var taskUnfinished = "Task yet to do: " + data[i].taskname;
+            var taskUnfinished = data[i].taskname;
             displayUnfinished.textContent = taskUnfinished;
             $("#taskField").append(displayUnfinished);
             var deleteBtnUnfinished = "<button class='deleteBtn' data-id='" + data[i].id + "'data-taskname='" + data[i].taskname + "'>Delete Task" + "</button>";
             $("#taskField").append(deleteBtnUnfinished);
-            var unfinishedBtn = "<button class='unfinishedBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" + data[i].complete + "'>Task Unfinished" + "</button>";
+            var unfinishedBtn = "<button class='unfinishedBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" + data[i].complete + "'>Task Complete" + "</button>";
             $("#taskField").append(unfinishedBtn);
+            numUnfinishedTasks++;
 
           } else {
 
             var displayComplete = document.createElement("div");
             displayComplete.id = data[i];
             displayComplete.className = "displayCompleteDiv";
-            var taskComplete = "Completed task: " + data[i].taskname;
+            var taskComplete = data[i].taskname;
             displayComplete.textContent = taskComplete;
             $("#taskField").append(displayComplete);
             var deleteBtnComplete = "<button class='deleteBtn' data-id='" + data[i].id + "'data-taskname='" + data[i].taskname + "'>Delete Task" + "</button>";
             $("#taskField").append(deleteBtnComplete);
-            var completeBtn = "<button class='completeBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" + data[i].complete + "'>Completed!" + "</button>";
+            var completeBtn = "<button class='completeBtn' id='toggleBtn' data-id='" + data[i].id + "'data-complete='" + data[i].complete + "'>Repeat Task?" + "</button>";
             $("#taskField").append(completeBtn);
           } // end else
         } // end for loop
+        if (data.length === 0) {
+          var displayTemp = document.createElement("div");
+          displayTemp.id = "displayTemp";
+          var taskNull = "Nothing to do? Add a task!";
+          displayTemp.textContent = taskNull;
+          $("#taskField").append(displayTemp);
+        } // end if
+        backgroundPic(numUnfinishedTasks);
       } // end success
     }); // end ajax
 } // end taskDisplay
+
+//-------------------------------------------------------------------------------------------------------------------
+
+function backgroundPic (num){
+  var bgImage = "background-image";
+    if(num >= 0 && num <= 3){
+      $('#backGroundImage').css(bgImage, "url(pictures/stressed_1.jpg)");
+    } else if (num >= 4 && num <= 6) {
+      $('#backGroundImage').css(bgImage, "url(pictures/stressed_2.jpg)");
+    } else if (num >= 7 && num <= 9) {
+      $('#backGroundImage').css(bgImage, "url(pictures/stressed_3.jpg)");
+    } else {
+      $('#backGroundImage').css(bgImage, "url(pictures/stressed_4.jpg)");
+    } // end else
+    numUnfinishedTasks = 0;
+} // end background pic func
 
 //-------------------------------------------------------------------------------------------------------------------
 
